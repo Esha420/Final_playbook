f0fb9a471575a5cdfb35bbbf37365c9e8f58da0e
Esha420 <pandey.isha420@gmail.com>
2024-06-14 11:14:12 +0545

    new Test

--- remove_node2.py
+++ remove_node2.py
@@ -1,15 +1,38 @@
+# import yaml
+
+# # Load the YAML file
+# with open('/tmp/kubespray/inventory/my-cluster/hosts.yml', 'r') as file:
+#     data = yaml.safe_load(file)
+
+# # Remove node2 from the kube_control_plane group
+# if 'children' in data and 'kube_control_plane' in data['children']:
+#     kube_control_plane = data['children']['kube_control_plane']
+#     if 'hosts' in kube_control_plane and 'node2' in kube_control_plane['hosts']:
+#         del kube_control_plane['hosts']['node2']
+
+# # Save the updated YAML file
+# with open('/tmp/kubespray/inventory/my-cluster/hosts.yml', 'w') as file:
+#     yaml.dump(data, file, default_flow_style=False)
+
 import yaml
 
-# Load the YAML file
-with open('/tmp/kubespray/inventory/my-cluster/hosts.yml', 'r') as file:
-    data = yaml.safe_load(file)
+def remove_node2(inventory_file):
+  """
+  Removes node2 from the kube_control_plane block in the inventory file.
 
-# Remove node2 from the kube_control_plane group
-if 'children' in data and 'kube_control_plane' in data['children']:
-    kube_control_plane = data['children']['kube_control_plane']
-    if 'hosts' in kube_control_plane and 'node2' in kube_control_plane['hosts']:
-        del kube_control_plane['hosts']['node2']
+  Args:
+    inventory_file (str): Path to the inventory YAML file.
+  """
+  with open(inventory_file, 'r') as f:
+    data = yaml.safe_load(f)
 
-# Save the updated YAML file
-with open('/tmp/kubespray/inventory/my-cluster/hosts.yml', 'w') as file:
-    yaml.dump(data, file, default_flow_style=False)
+  data['all']['children']['kube_control_plane']['hosts'].pop('node2', None)
+
+  with open(inventory_file, 'w') as f:
+    yaml.dump(data, f, default_flow_style=False)
+
+# Example usage
+inventory_file = "/tmp/kubespray/inventory/my-cluster/hosts.yml"  # Replace with your actual inventory file path
+remove_node2(inventory_file)
+
+print(f"Node2 removed from kube_control_plane block in {inventory_file}")
