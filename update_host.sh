165f8a018c4a335d9d0d440f3239e4ae0de29e6d
Esha Pandey <esha.pandey@pdc01.lisnepal.com.np>
2024-06-14 14:09:26 +0545

    changes

--- update_host.sh
+++ update_host.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-hosts_file="tmp/kubespray/inventory/my-cluster/hosts.yml"
+hosts_file="/tmp/kubespray/inventory/my-cluster/hosts.yml"
 
 # Remove node2 from the block
 remove_node2() {
-    sed -i '/node2:/,/node3:/d' $hosts_file
+    sed -i '/kube_control_plane:/,/kube_node:/{
+            /node2:/d
+        }' $hosts_file
 }
+remove_node2
 
 # Print the updated file
 cat $hosts_file
